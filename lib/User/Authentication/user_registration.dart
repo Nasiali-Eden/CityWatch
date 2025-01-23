705f38dbf16022a03ebbdf6191ed8bc30d6a69f4
nasiali-eden <nasieden0@gmail.com>
2025-01-23 20:27:45 +0300

    User Registration

---
+++ lib/User/Authentication/user_registration.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class UserRegistration extends StatefulWidget {
+  const UserRegistration({super.key});
+
+  @override
+  State<UserRegistration> createState() => _UserRegistrationState();
+}
+
+class _UserRegistrationState extends State<UserRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(56.0), // Set the height of the AppBar
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple[700],
+                    boxShadow: [
+                        BoxShadow(
+                            color: Colors.white.withAlpha((0.05 * 255).toInt()), // Shadow color with opacity
+                            blurRadius: 4.0, // Adjust the blur radius
+                            offset: Offset(0, 3), // Position of the shadow
+                        ),
+                    ],
+                ),
+                child: AppBar(
+                    backgroundColor:
+                    Colors.transparent, // Make the AppBar background transparent
+                    elevation: 0, // Remove default shadow
+                    title: Text(
+                        'CityWatch',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22,
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        body: Center(
+        child: Text('CityWatch'),
+      ),
+    );
+  }
+}
