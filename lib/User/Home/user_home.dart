6b246947f6e59756da14d6e36878f506e9af5e1f
nasiali-eden <nasieden0@gmail.com>
2025-01-30 22:59:10 +0300

    BG colors

--- lib/User/Home/user_home.dart
+++ lib/User/Home/user_home.dart
@@ -27,125 +27,137 @@ class _UserHomeState extends State<UserHome> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: pages[currentPage], // Display the current page
-      bottomNavigationBar: BottomAppBar(
-        elevation: 100.0,
-        color: Colors.white, // Background color of the BottomAppBar
-        height: 85.0,
-
-        child: Row(
-          mainAxisAlignment:
-          MainAxisAlignment.spaceEvenly, // Evenly distribute icons
-          children: <Widget>[
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.auto_awesome_mosaic_outlined,
-                    color: currentPage == 0
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                  iconSize: 25.0,
-                  onPressed: () {
-                    setState(() {
-                      currentPage = 0;
-                    });
-                  },
-                ),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    fontSize: 11.0,
-                    color: currentPage == 0
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.map_outlined,
-                    color: currentPage == 1
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                  iconSize: 25.0,
-                  onPressed: () {
-                    setState(() {
-                      currentPage = 1;
-                    });
-                  },
-                ),
-                Text(
-                  'Incidents',
-                  style: TextStyle(
-                    fontSize: 11.0,
-                    color: currentPage == 1
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.help,
-                    color: currentPage == 2
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                  iconSize: 25.0,
-                  onPressed: () {
-                    setState(() {
-                      currentPage = 2;
-                    });
-                  },
-                ),
-                Text(
-                  'Help',
-                  style: TextStyle(
-                    fontSize: 11.0,
-                    color: currentPage == 2
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.person,
-                    color: currentPage == 3
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                  iconSize: 25.0,
-                  onPressed: () {
-                    setState(() {
-                      currentPage = 3;
-                    });
-                  },
-                ),
-                Text(
-                  'Profile',
-                  style: TextStyle(
-                    fontSize: 11.0,
-                    color: currentPage == 3
-                        ? const Color.fromRGBO(124, 77, 255, 1.0)
-                        : const Color.fromRGBO(40, 40, 40, 1),
-                  ),
-                )
-              ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.deepPurpleAccent.withAlpha((0.05 * 255).toInt()), // Shadow color
+              spreadRadius: 2, // Spread radius
+              blurRadius: 10, // Blur radius
+              offset: const Offset(0, -2), // Shadow position
             ),
           ],
+        ),
+        child: BottomAppBar(
+          elevation: 100.0,
+          color: Colors.white, // Background color of the BottomAppBar
+          height: 85.0,
+
+          child: Row(
+            mainAxisAlignment:
+            MainAxisAlignment.spaceEvenly, // Evenly distribute icons
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.auto_awesome_mosaic_outlined,
+                      color: currentPage == 0
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                    iconSize: 25.0,
+                    onPressed: () {
+                      setState(() {
+                        currentPage = 0;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      fontSize: 11.0,
+                      color: currentPage == 0
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.map_outlined,
+                      color: currentPage == 1
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                    iconSize: 25.0,
+                    onPressed: () {
+                      setState(() {
+                        currentPage = 1;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Incidents',
+                    style: TextStyle(
+                      fontSize: 11.0,
+                      color: currentPage == 1
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.help,
+                      color: currentPage == 2
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                    iconSize: 25.0,
+                    onPressed: () {
+                      setState(() {
+                        currentPage = 2;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Help',
+                    style: TextStyle(
+                      fontSize: 11.0,
+                      color: currentPage == 2
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.person,
+                      color: currentPage == 3
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                    iconSize: 25.0,
+                    onPressed: () {
+                      setState(() {
+                        currentPage = 3;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 11.0,
+                      color: currentPage == 3
+                          ? const Color.fromRGBO(124, 77, 255, 1.0)
+                          : const Color.fromRGBO(40, 40, 40, 1),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
