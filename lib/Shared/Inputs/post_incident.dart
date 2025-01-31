70d0f11d9130073a8d9139d2d0436e2a89023097
nasiali-eden <nasieden0@gmail.com>
2025-01-31 23:21:29 +0300

    Incident

--- lib/Shared/Inputs/post_incident.dart
+++ lib/Shared/Inputs/post_incident.dart
@@ -8,6 +8,51 @@ class PostIncident extends StatefulWidget {
 }
 
 class _PostIncidentState extends State<PostIncident> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _headlineController = TextEditingController();
+
+  String _headline = '';
+  String _description = '';
+
+  void _onHeadlineChanged(String value) {
+    setState(() {
+      _headline = value;
+    });
+  }
+
+  void _onDescriptionChanged(String value) {
+    setState(() {
+      _description = value;
+    });
+  }
+
+  final List<String> _selectedTypes = [];
+
+  void _toggleType(String type) {
+    setState(() {
+      if (_selectedTypes.contains(type)) {
+        // If the type is already selected, deselect it
+        _selectedTypes.remove(type);
+      } else {
+        // If the type is not selected, clear the list and select the new type
+        _selectedTypes.clear();
+        _selectedTypes.add(type);
+      }
+    });
+  }
+
+  void _toggleLocation(String location) {
+    setState(() {
+      if (_selectedTypes.contains(location)) {
+        _selectedTypes.remove(location);
+      } else {
+        _selectedTypes.clear();
+        _selectedTypes.add(location);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +87,160 @@ class _PostIncidentState extends State<PostIncident> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: _headlineController,
+                decoration: InputDecoration(
+                  labelText: 'Headline',
+                  labelStyle: const TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w300),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade100),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54, width: 0.1),
+                  ),
+                ),
+                onChanged: _onHeadlineChanged,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please add the headline';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  _buildStyleRadioButton('Health'),
+                  _buildStyleRadioButton('Fire'),
+                  _buildStyleRadioButton('Floods'),
+                  _buildStyleRadioButton('Wildlife'),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: const TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w300),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade100),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54, width: 0.1),
+                  ),
+                ),
+                onChanged: _onDescriptionChanged,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please add Description';
+                  }
+                  return null;
+                },
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  _buildLocationRadioButton('Use Current Location'),
+                  _buildLocationRadioButton('Use Custom Location'),
+                ],
+              ),
+              SizedBox(height: 15,),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Upload Images',
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                ),
+              ),
+              SizedBox(height: 35,),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Post',
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStyleRadioButton(String type) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child: FilterChip(
+        label: Text(
+          type,
+          // Conditional text color based on selection
+          style: TextStyle(
+            color: _selectedTypes.contains(type)
+                ? Colors.white
+                : Colors.deepPurple,
+          ),
+        ),
+        selected: _selectedTypes.contains(type),
+        onSelected: (selected) {
+          _toggleType(type);
+        },
+        backgroundColor: Colors.white, // Background color when not selected
+        selectedColor: Colors.deepPurple, // Background color when selected
+        side: _selectedTypes.contains(type)
+            ? BorderSide.none // No border when selected
+            : BorderSide(
+                color: Colors.deepPurple, // Border color when not selected
+              ),
+      ),
+    );
+  }
+
+  Widget _buildLocationRadioButton(String location) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child: FilterChip(
+        label: Text(
+          location,
+          // Conditional text color based on selection
+          style: TextStyle(
+            color: _selectedTypes.contains(location)
+                ? Colors.white
+                : Colors.deepPurple,
+          ),
+        ),
+        selected: _selectedTypes.contains(location),
+        onSelected: (selected) {
+          _toggleLocation(location);
+        },
+        backgroundColor: Colors.white, // Background color when not selected
+        selectedColor: Colors.deepPurple, // Background color when selected
+        side: _selectedTypes.contains(location)
+            ? BorderSide.none // No border when selected
+            : BorderSide(
+                color: Colors.deepPurple, // Border color when not selected
+              ),
       ),
     );
   }
