0e78c8115ad2ea14b1e9d8a6d0e47943593257ff
nasiali-eden <nasieden0@gmail.com>
2025-03-18 19:40:40 +0300

    Add

---
+++ android/app/src/main/java/com/example/city_watch/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.city_watch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+import com.google.firebase.FirebaseApp;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        boolean isDebuggable = (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+
+        if (isDebuggable) { // Check if app is in debug mode
+            FirebaseApp firebaseApp = FirebaseApp.getInstance(); // Get Firebase instance
+
+            SharedPreferences prefs = getSharedPreferences(
+                    "com.google.firebase.appcheck.debug.store." + firebaseApp.getPersistenceKey(),
+                    Context.MODE_PRIVATE
+            );
+
+            prefs.edit()
+                    .putString(
+                            "com.google.firebase.appcheck.debug.DEBUG_SECRET",
+                            "31F991C6-0702-44C0-AF76-1ACFEDB6E1C8"
+                    )
+                    .apply();
+        }
+    }
+}
+
