595306a03f915e6441d99c8fc8ebf706bdee22ba
nasiali-eden <nasieden0@gmail.com>
2025-03-19 18:45:04 +0300

    Help

--- lib/User/Home/Help/user_help.dart
+++ lib/User/Home/Help/user_help.dart
@@ -47,15 +47,21 @@ class _UserHelpState extends State<UserHelp> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text("HelpLines",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
+                style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black)),
+            SizedBox(height: 18,),
             _buildButton(context, "Police Services"),
             _buildButton(context, "Ambulance Services"),
             _buildButton(context, "Kenya Red Cross"),
             SizedBox(height: 16),
             Text("Nearby Resources",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black)),
+            SizedBox(height: 18),
             _buildButton(context, "Refugee Camps"),
             _buildButton(context, "Government Shelters"),
             _buildButton(context, "Free Medical Services"),
@@ -75,14 +81,14 @@ class _UserHelpState extends State<UserHelp> {
       child: Container(
         width: double.infinity,
         padding: EdgeInsets.symmetric(vertical: 12),
-        margin: EdgeInsets.only(bottom: 8),
+        margin: EdgeInsets.only(bottom: 15),
         decoration: BoxDecoration(
           color: Colors.grey[200],
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
           child: Text(text,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300)),
         ),
       ),
     );
