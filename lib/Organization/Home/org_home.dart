9ef7b9fb0d29778e72572a283d6b16bbac735515
nasiali-eden <nasieden0@gmail.com>
2025-03-27 08:39:08 +0300

    Home

--- lib/Organization/Home/org_home.dart
+++ lib/Organization/Home/org_home.dart
@@ -1,5 +1,4 @@
 import 'package:city_watch/Organization/Home/Dashboard/org_dashboard.dart';
-import 'package:city_watch/Organization/Home/OurTeam/org_team.dart';
 import 'package:city_watch/Organization/Home/Profile/org_profile.dart';
 import 'package:city_watch/Organization/Home/Volunteers/org_volunteers.dart';
 import 'package:city_watch/User/Home/Reports/user_reports.dart';
@@ -27,7 +26,6 @@ class _OrganizationHomeState extends State<OrganizationHome> {
     pages = [
       OrgDashboard(key: dashboardKey),
       const UserReports(),
-      const OrgTeam(),
       const OrgVolunteers(),
       const OrgProfile(),
     ];
@@ -37,7 +35,6 @@ class _OrganizationHomeState extends State<OrganizationHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // Use an IndexedStack to preserve the state of each page
       body: IndexedStack(
         index: currentPage,
         children: pages,
@@ -46,8 +43,7 @@ class _OrganizationHomeState extends State<OrganizationHome> {
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color:
-              Colors.deepPurpleAccent.withAlpha((0.05 * 255).toInt()),
+              color: Colors.deepPurpleAccent.withAlpha((0.05 * 255).toInt()),
               spreadRadius: 2,
               blurRadius: 10,
               offset: const Offset(0, -2),
@@ -61,153 +57,49 @@ class _OrganizationHomeState extends State<OrganizationHome> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              // Dashboard icon
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.apps,
-                      color: currentPage == 0
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                    iconSize: 25.0,
-                    onPressed: () {
-                      if (currentPage == 0) {
-                        // If already on Dashboard, reset its nested navigator.
-                        dashboardKey.currentState?.popToRoot();
-                      } else {
-                        setState(() {
-                          currentPage = 0;
-                        });
-                      }
-                    },
-                  ),
-                  Text(
-                    'Dashboard',
-                    style: TextStyle(
-                      fontSize: 11.0,
-                      color: currentPage == 0
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                  )
-                ],
-              ),
-              // Other bottom nav icons...
-              Column(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.bolt_outlined,
-                      color: currentPage == 1
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                    iconSize: 25.0,
-                    onPressed: () {
-                      setState(() {
-                        currentPage = 1;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Incidents',
-                    style: TextStyle(
-                      fontSize: 11.0,
-                      color: currentPage == 1
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.people,
-                      color: currentPage == 2
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                    iconSize: 25.0,
-                    onPressed: () {
-                      setState(() {
-                        currentPage = 2;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Our Team',
-                    style: TextStyle(
-                      fontSize: 11.0,
-                      color: currentPage == 2
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.construction,
-                      color: currentPage == 3
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                    iconSize: 25.0,
-                    onPressed: () {
-                      setState(() {
-                        currentPage = 3;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Volunteers',
-                    style: TextStyle(
-                      fontSize: 11.0,
-                      color: currentPage == 3
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.person_2_outlined,
-                      color: currentPage == 4
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                    iconSize: 25.0,
-                    onPressed: () {
-                      setState(() {
-                        currentPage = 4;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Profile',
-                    style: TextStyle(
-                      fontSize: 11.0,
-                      color: currentPage == 4
-                          ? const Color.fromRGBO(124, 77, 255, 1.0)
-                          : const Color.fromRGBO(40, 40, 40, 1),
-                    ),
-                  )
-                ],
-              ),
+              _buildNavItem(Icons.apps, "Dashboard", 0),
+              _buildNavItem(Icons.bolt_outlined, "Incidents", 1),
+              _buildNavItem(Icons.construction, "Volunteers", 2),
+              _buildNavItem(Icons.person_2_outlined, "Profile", 3),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, int index) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(
+            icon,
+            color: currentPage == index
+                ? const Color.fromRGBO(124, 77, 255, 1.0)
+                : const Color.fromRGBO(40, 40, 40, 1),
+          ),
+          iconSize: 25.0,
+          onPressed: () {
+            if (index == 0 && currentPage == 0) {
+              dashboardKey.currentState?.popToRoot();
+            } else {
+              setState(() {
+                currentPage = index;
+              });
+            }
+          },
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 11.0,
+            color: currentPage == index
+                ? const Color.fromRGBO(124, 77, 255, 1.0)
+                : const Color.fromRGBO(40, 40, 40, 1),
+          ),
+        )
+      ],
     );
   }
 }
